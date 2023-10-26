32973f0dc3a0b80952cb5814efd15958d04140bf
Udagawakouta <115062885+Udagawakouta@users.noreply.github.com>
2023-10-26 16:53:01 +0900

    PG3_01_03　完

--- main.cpp
+++ main.cpp
@@ -1,7 +1,77 @@
 #include <stdio.h>
+#include <Windows.h>
+#include <time.h>
 
-int main() {
-	
+typedef void (*PFunc)(int*);
 
-	return(0);
+#pragma region コールバック関数
+
+// コールバック関数
+void DispResult(int* s)
+{
+	printf("%d秒待つ\n", *s);
 }
+
+void setTimeout(PFunc p, int& second)
+{
+	p(&second);
+	Sleep(second * 1000);
+}
+#pragma endregion 
+
+
+#pragma region 関数ポインタ
+int main()
+{
+	srand((unsigned int)time(NULL));
+
+	int n = 0;
+	int m = 3;
+	int dice;
+	dice = rand() % 6 + 1;
+
+	PFunc p;
+	p = DispResult;
+
+	printf("0か1を入力してください\n");
+	scanf_s("%d", &n);
+
+	if (n == 0)
+	{
+		if (dice % 2 == 0)
+		{
+			setTimeout(p, m);
+
+			printf("正解\n");
+
+		}
+		else
+		{
+			setTimeout(p, m);
+
+			printf("不正解\n");
+
+		}
+	}
+	else
+	{
+		if (dice % 2 == 1)
+		{
+			setTimeout(p, m);
+
+			printf("正解\n");
+		}
+		else
+		{
+			setTimeout(p, m);
+
+			printf("不正解\n");
+
+		}
+	}
+
+	printf("正解は %d でした", dice);
+
+	return 0;
+}
+#pragma endregion
