6861ba352866933db9b6b360f80a755f4d9fd217
Udagawakouta <115062885+Udagawakouta@users.noreply.github.com>
2023-11-07 12:49:19 +0900

    終わり

--- main.cpp
+++ main.cpp
@@ -1,57 +1,12 @@
 #include <stdio.h>
-#include <Windows.h>
-#include <time.h>
-#include<functional>
+#include "Enemy.h"
 
-typedef void (*PFunc)(int*);
-
-#pragma region コールバック関数
-
-// コールバック関数
-void DispResult(int* s)
-{
-	printf("%d秒待つ\n", *s);
-}
-
-void setTimeout(std::function<void(int*)> p, int& second)
-{
-	p(&second);
-	Sleep(second * 1000);
-}
-#pragma endregion 
-
-
-#pragma region 関数ポインタ
 int main()
 {
-	std::function<void(int*)> fx = [](int* s) {printf("%d秒待つ\n", *s); };
-
-	srand((unsigned int)time(NULL));
-
-	int evenNumber = 0;
-	int oddNumber = 3;
-	int dice;
-	dice = rand() % 6 + 1;
-
-	PFunc p;
-	p = DispResult;
-
-	printf("0か1を入力してください\n");
-	scanf_s("%d", &evenNumber);
-
-	setTimeout(p, oddNumber);
-
-	if (evenNumber == dice % 2)
-	{
-		printf("正解\n");
-	}
-	else
-	{
-		printf("不正解\n");
-	}
-
-	printf("正解は %d でした", dice);
+	Enemy enemy;
+	enemy.Update();
+	enemy.Update();
+	enemy.Update();
 
 	return 0;
 }
-#pragma endregion
